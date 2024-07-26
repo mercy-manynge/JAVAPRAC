e927ff1572e81f26713a7cee72bc55bda4045fb5
Mercy Manyange <mercyttongesai@gmail.com>
2024-07-26 22:32:57 +0200

    learning how a for loop works

--- LambdaExp/ArrayVsArrayList/ArrayLists.java
+++ LambdaExp/ArrayVsArrayList/ArrayLists.java
@@ -18,7 +18,9 @@ public class ArrayLists {
         };
         for (int i = 0; i < 3; i++){
             System.out.println(numbers[i][i]);
+            System.out.println("xxx xxx xxx");
             System.out.println(numbers[2][2]);
+            System.out.println("/////");
             for(int j = 0; j < 3; j++){
             System.out.println(numbers[i][j]);
             }
--- LambdaExp/ReadWrite/Dog.java
+++ LambdaExp/ReadWrite/Dog.java
@@ -12,5 +12,17 @@ public class Dog {
    public static void main(String[] args) {
     Dog myDog = new Dog("Jimmy", 3);//Pass the name as a parameter to our Dog constructor
     System.out.println(myDog.name + " " + myDog.age);
+
+    
+    
+          For: for (int i = 0; i < 10; i++) {
+              for (int j = 0; j < 10; j++) {
+                  if (i + j > 10) break For;
+                  System.out.println("hello");
+              }
+          }
+   
+  
+  
    } 
 }
