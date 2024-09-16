58dfe4fcb8c05558fb0c0da45d4f5efcba7af5b8
Mercy Manyange <mercyttongesai@gmail.com>
2024-09-16 10:15:59 +0200

    Continue learning abstract classes

--- LambdaExp/Inheritance/AbstractMC.java
+++ LambdaExp/Inheritance/AbstractMC.java
@@ -4,6 +4,7 @@ public abstract class AbstractMC {
     int a = 7;
     int b = 8;
     abstract String book();
+    abstract int say();
 
     public int books() {
         return a + b;
@@ -11,8 +12,13 @@ public abstract class AbstractMC {
 }
 final abstract class Apple extends AbstractMC{}// this will throw an excepton because final and bract can not be used in the same class 
 //A class can be either final or abtract not both
-class abstract MyBook extends AbstractMC{}// this class will not compile because abstract methods can not extend themselves 
-class Addition extends AbstractMC{
+ abstract class MyBook extends AbstractMC{//Compiles because abtract classes can only be extended by abtract classes and are not required to inherit any abstract methods from the parent
+     public int say(){
+        return 2;
+     }
+     
+ }
+class Addition extends MyBook{// This is a concrete class and is required to inherit all methods of the abstract class
     //private String book(){//This does not compile because it reduces the visibility form the parent class
         //System.out.println("this is my book");
     //}
@@ -20,3 +26,5 @@ class Addition extends AbstractMC{
         return "My book is covered";
     }
 }
+// A non abstract class should not contain an abstract method
+//The concrete class Addition is not required to provide an implementation for an abstract method if an intermediate abstract class provides the method
