f6e56dae9cbe97c73fe5b0e75faf68355290b256
Mercy Manyange <mercyttongesai@gmail.com>
2024-09-17 16:49:59 +0200

    Learn how if then else statement work

---
+++ LambdaExp/ConditionalStatements/IfThen.java
@@ -0,0 +1,15 @@
+package ConditionalStatements;
+
+public class IfThen {
+    public static void main(String[] args) {
+    int a = 2; 
+    int b = 6;
+    if (b < a)//Curly braces are not required if there is one return statement
+    /*The below is the then Statement */
+    System.out.println("result");//If the conditional block matches the requirement return the result
+    else // If it doesn't, return the noresult statement
+    System.out.println("No result");
+    
+    }
+    
+}
