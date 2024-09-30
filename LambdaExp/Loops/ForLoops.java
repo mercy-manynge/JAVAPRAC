7a79c4acb910452c72ab8130208267c9e17eb1fc
Mercy Manyange <mercyttongesai@gmail.com>
2024-09-30 11:20:44 +0200

    The for loop

--- LambdaExp/Loops/ForLoops.java
+++ LambdaExp/Loops/ForLoops.java
@@ -1,5 +1,13 @@
 package Loops;
 
 public class ForLoops {
+    public static void main(String [] args) {
+        int x = 12;// Class variable
+        for (int a = 0; a < 10; a++){// the variable created here is limited to the scope of the for loop body only
+        System.out.println("a is " + a);
+    }
+        System.out.println("The end of the for loop " + x);// x can be accessed here
+    }
     
-}
+    }
+
